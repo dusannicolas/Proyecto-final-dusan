69c0e1523fb76c215e1cfc04f0d8b66011015c84
dusannicolas <dusanmatulic@gmail.com>
2023-04-25 18:58:42 -0300

    subida
    subir mis archivos

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function abrirRecetas(){
+    window.open("recetas.html")
+}
+
+const recetas = [
+    {
+        "imagen":"",
+        "nombre": "Calugones base dátiles",
+        "preparacion": "Se procesan los dátiles sin agua hasta que se muelan y se mezcla con los demás ingredientes, en la misma procesadora. Queda una mezcla un poco pegote así es que con las manos húmedas esparces la mezcla en un molde con papel de hornear de 20x20 más o menos. Y al refrigerador por un par de horas. Como queda un poco pegote al cortar los cuadrados los pasas por coco rallado blanco o tostado o almendras molidas, etc. Juega con la receta y las opciones de cubrir los calugones. Igual puedes hacer bolitas si prefieres y las pones en pirotines." 
+    },
+
+    {
+        "imagen":"",
+        "nombre": "Hummus",
+        "preparacion": "garbanzos"
+    }
+];
+
